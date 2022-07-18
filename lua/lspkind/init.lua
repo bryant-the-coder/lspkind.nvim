630c6ac2660b88eff2ae579c7e10a7a975aaec67
bryant-the-coder <bryantfoo99@gmail.com>
2022-07-18 20:06:39 +0800

    Update init.lua

--- lua/lspkind/init.lua
+++ lua/lspkind/init.lua
@@ -3,130 +3,93 @@ local fmt = string.format
 
 local kind_presets = {
   default = {
--- if you change or add symbol here
--- replace corresponding line in readme
-    Text = "",
-    Method = "",
-    Function = "",
-    Constructor = "",
-    Field = "ﰠ",
-    Variable = "",
-    Class = "ﴯ",
-    Interface = "",
-    Module = "",
-    Property = "ﰠ",
-    Unit = "塞",
-    Value = "",
-    Enum = "",
-    Keyword = "",
-    Snippet = "",
-    Color = "",
-    File = "",
-    Reference = "",
-    Folder = "",
-    EnumMember = "",
-    Constant = "",
-    Struct = "פּ",
-    Event = "",
-    Operator = "",
-    TypeParameter = ""
-  },
-  codicons = {
-    Text = "",
-    Method = "",
-    Function = "",
-    Constructor = "",
-    Field = "",
-    Variable = "",
-    Class = "",
-    Interface = "",
-    Module = "",
-    Property = "",
-    Unit = "",
-    Value = "",
-    Enum = "",
-    Keyword = "",
-    Snippet = "",
-    Color = "",
-    File = "",
-    Reference = "",
-    Folder = "",
-    EnumMember = "",
-    Constant = "",
-    Struct = "",
-    Event = "",
-    Operator = "",
-    TypeParameter = "",
+    Class = "  ",
+    Color = "  ",
+    Constant = "  ",
+    Constructor = "  ",
+    Enum = " 了",
+    EnumMember = "  ",
+    Event = "  ",
+    Field = " ﰠ ",
+    -- File = " ",
+    File = "  ",
+    Folder = "  ",
+    Function = "  ",
+    Interface = " ﰮ ",
+    Keyword = "  ",
+    Method = " ƒ ",
+    Module = "  ",
+    Operator = "  ",
+    Property = "  ",
+    Reference = " ",
+    -- Snippet = "  ",
+    -- Snippet = "  ",
+    -- Snippet = "  ",
+    -- Snippet = " >",
+    Snippet = "  ",
+    Struct = " ",
+    Text = "  ",
+    TypeParameter = "",
+    Unit = " 塞",
+    Value = "  ",
+    Variable = "  ",
   },
 }
 
 local kind_order = {
-  'Text', 'Method', 'Function', 'Constructor', 'Field', 'Variable', 'Class', 'Interface', 'Module',
-  'Property', 'Unit', 'Value', 'Enum', 'Keyword', 'Snippet', 'Color', 'File', 'Reference', 'Folder',
-  'EnumMember', 'Constant', 'Struct', 'Event', 'Operator', 'TypeParameter'
+  "Text",
+  "Method",
+  "Function",
+  "Constructor",
+  "Field",
+  "Variable",
+  "Class",
+  "Interface",
+  "Module",
+  "Property",
+  "Unit",
+  "Value",
+  "Enum",
+  "Keyword",
+  "Snippet",
+  "Color",
+  "File",
+  "Reference",
+  "Folder",
+  "EnumMember",
+  "Constant",
+  "Struct",
+  "Event",
+  "Operator",
+  "TypeParameter",
 }
 local kind_len = 25
 
-local function get_symbol(kind) 
-    local symbol = lspkind.symbol_map[kind]
-    return symbol or ''
-end
-
-local modes = { 
-    ['text'] = function(kind)
-        return kind
-    end, 
-    ['text_symbol'] = function(kind)
-        local symbol = get_symbol(kind)
-        return fmt("%s %s", kind, symbol)
-    end, 
-    ['symbol_text'] = function(kind)
-        local symbol = get_symbol(kind)
-        return fmt("%s %s", symbol, kind)
-    end, 
-    ['symbol']  = function(kind)
-        local symbol = get_symbol(kind)
-        return fmt("%s", symbol)
-    end 
-}
-
 -- default true
--- deprecated
 local function opt_with_text(opts)
-  return opts == nil or opts['with_text'] == nil or opts['with_text']
-end
-
--- default 'symbol'
-local function opt_mode(opts) 
-    local mode = 'symbol'
-    if opt_with_text(opts) and opts ~= nil and opts['mode'] == nil then
-        mode = 'symbol_text'
-    elseif opts ~= nil and opts['mode'] ~= nil then
-        mode = opts['mode']
-    end
-    return mode
+  return opts == nil or opts["with_text"] == nil or opts["with_text"]
 end
 
 -- default 'default'
 local function opt_preset(opts)
   local preset
-  if opts == nil or opts['preset'] == nil then
-    preset = 'default'
+  if opts == nil or opts["preset"] == nil then
+    preset = "default"
   else
-    preset = opts['preset']
+    preset = opts["preset"]
   end
   return preset
 end
 
 function lspkind.init(opts)
-  if opts~=nil and opts['with_text'] ~= nil then
-    vim.api.nvim_command("echoerr 'DEPRECATED replaced by mode option.'")
-  end
   local preset = opt_preset(opts)
 
   local symbol_map = kind_presets[preset]
-  lspkind.symbol_map = (opts and opts['symbol_map'] and
-                       vim.tbl_extend('force', symbol_map, opts['symbol_map'])) or symbol_map
+  lspkind.symbol_map = (
+      opts
+          and opts["symbol_map"]
+          and vim.tbl_extend("force", symbol_map, opts["symbol_map"])
+      ) or symbol_map
 
   local symbols = {}
   local len = kind_len
@@ -135,8 +98,8 @@ function lspkind.init(opts)
     symbols[i] = lspkind.symbolic(name, opts)
   end
 
-  for k,v in pairs(symbols) do
-    require('vim.lsp.protocol').CompletionItemKind[k] = v
+  for k, v in pairs(symbols) do
+    require("vim.lsp.protocol").CompletionItemKind[k] = v
   end
 end
 
@@ -144,15 +107,15 @@ lspkind.presets = kind_presets
 lspkind.symbol_map = kind_presets.default
 
 function lspkind.symbolic(kind, opts)
-  local mode = opt_mode(opts)
-  local formatter = modes[mode]
+  local with_text = opt_with_text(opts)
 
-  -- if someone enters an invalid mode, default to symbol
-  if formatter == nil then
-      formatter = modes['symbol']
+  local symbol = lspkind.symbol_map[kind]
+  if with_text == true then
+    symbol = symbol and (symbol .. " ") or ""
+    return fmt("%s%s", symbol, kind)
+  else
+    return symbol
   end
-
-  return formatter(kind)
 end
 
 function lspkind.cmp_format(opts)
@@ -164,10 +127,6 @@ function lspkind.cmp_format(opts)
   end
 
   return function(entry, vim_item)
-    if opts.before then
-          vim_item = opts.before(entry, vim_item)
-    end
-        
     vim_item.kind = lspkind.symbolic(vim_item.kind, opts)
 
     if opts.menu ~= nil then
@@ -175,10 +134,17 @@ function lspkind.cmp_format(opts)
     end
 
     if opts.maxwidth ~= nil then
-        vim_item.abbr = string.sub(vim_item.abbr, 1, opts.maxwidth)
+      vim_item.abbr = string.sub(vim_item.abbr, 1, opts.maxwidth)
     end
 
     return vim_item
+  end
+end
+
+function lspkind.setup()
+  local kinds = vim.lsp.protocol.CompletionItemKind
+  for i, kind in ipairs(kinds) do
+    kinds[i] = lspkind.icons[kind] or kind
   end
 end
 
